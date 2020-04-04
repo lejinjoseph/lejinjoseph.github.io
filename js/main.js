0b03c8e4230c56aa3ead3b7bb96f829cea5a92b3
Dijo Davis <heloworld.d@gmail.com>
2020-04-04 12:23:38 +0530

    Owl Carousel library - unused - removed

--- js/main.js
+++ js/main.js
@@ -124,14 +124,14 @@ jQuery(document).ready(function( $ ) {
   });
 
   // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center:true,
-    responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
-    }
-  });
+  // $(".gallery-carousel").owlCarousel({
+  //   autoplay: true,
+  //   dots: true,
+  //   loop: true,
+  //   center:true,
+  //   responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
+  //   }
+  // });
 
   // Buy tickets select the ticket type on click
   $('#buy-ticket-modal').on('show.bs.modal', function (event) {
