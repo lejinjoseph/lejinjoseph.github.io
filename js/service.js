e270eba6675dc1600a174933de0e8c030e6a3bfd
Dijo Davis <heloworld.d@gmail.com>
2020-04-05 02:33:10 +0530

    Service integration - languages + days

---
+++ js/service.js
@@ -0,0 +1,84 @@
+jQuery(document).ready(function ($) {
+    csService.init();
+});
+
+var csService = {
+    url: 'http://catholicstreamlive-env.eba-mh2niqse.ap-south-1.elasticbeanstalk.com',
+
+    init: function (params) {
+        csService.getLanguanges();
+    },
+
+    capitalizeString: function (string) {
+        return string.replace(/\b\w/g, function (l) { return l.toUpperCase() })
+    },
+
+    getLanguanges: function () {
+        $.get(csService.url + '/getLanguages', function (data) {
+            csService.displayLanguages(data);
+            csService.getDays();
+        })
+            .fail(function () {
+                console.log('failed to get languages!');
+            });
+    },
+
+    getDays: function () {
+        $.get(csService.url + '/getDays', csService.displayDays)
+            .fail(function () {
+                console.log('failed to get days!');
+            });
+    },
+
+    getSchedule: function () {
+
+    },
+
+    displayLanguages: function (data) {
+        $.each(data, function (index, orginalLang) {
+            language = csService.capitalizeString(orginalLang);
+            // add language button
+            $("#langSelection").append(
+                `<li class="nav-item lej-padding">
+                <a class="nav-link" href="#${language}Mass" role="tab" data-toggle="tab"
+                  aria-selected="true">${language}</a>
+              </li>`
+            );
+
+            //add schedule tab panel
+            $("#holyMass").append(
+                `<div id="${language}Mass" data-mass-lang="${orginalLang}" role="tabpanel" class="tab-pane fade">
+                    <h3 class="text-center text-capitalize font-weight-bold text-danger">${language} Holy Mass</h3>
+                    <p>All timings are in IST (GMT+5.30).
+                        <span class="instructions badge badge-info badge-pill my-1" onclick="openModal()"><i
+                            class="fa fa-book pr-1"></i>Guidelines</span>
+                    </p>
+                    <ul class="daySelection nav nav-tabs nav-fill" role="tablist">
+                    </ul>
+                    <div class="tab-content row justify-content-center"></div>
+                </div>`
+            );
+        })
+
+    },
+
+    displayDays: function (days) {
+        $.each($("ul.daySelection"), function (index, dom) {
+            var language = $(dom).parents(".tab-pane").attr("id");
+            var tabContent = $(dom).siblings("div.tab-content");
+            $.each(days, function (index, day) {
+                var langDayId = (day.name + language).replace(' ', '-');
+                $(dom).append(
+                    `<li class="nav-item lej-padding">
+                        <a class="nav-link" href="#${langDayId}" data-day-id="${day.name}" role="tab" data-toggle="tab">${csService.capitalizeString(day.displayName)}</a>
+                    </li>`
+                );
+
+                $(tabContent).append(
+                    `<div  role="tabpanel" class="col-lg-9  tab-pane fade" id="${langDayId}"> ${langDayId} Loading.. </div>`
+                );
+            })
+        });
+
+    }
+}
