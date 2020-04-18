0f113eddbad5552bbb5b1b1c40b95353e1617f71
Dijo Davis <dijodevpd@gmail.com>
2020-04-19 01:26:54 +0530

    Youtube modal - first - flow completed

--- dist/js/app.js
+++ dist/js/app.js
@@ -303,6 +303,12 @@ jQuery(document).ready(function ($) {
 
 var csVideo = {
 
+    testChannelId: "UCxqxxgXZxSwLpPk6pCC_XJA",
+
+    youtubeApiUrl: "https://www.googleapis.com/youtube/v3/",
+
+    apiKey: "AIzaSyAvcxy7qbSPvZhVn8ueAqM_oMijj08SAmY",
+
     videoModal: null,
 
     registerEvents: function () {
@@ -310,6 +316,7 @@ var csVideo = {
         csVideo.videoModal = $("#videoModal");
 
         $("body").on("click", "button.watchStream", csVideo.onWatch);
+        $('#videoModal').on('hidden.bs.modal', csVideo.resetYoutubeModal)
     },
 
     parseUrl: function (url) {
@@ -320,28 +327,47 @@ var csVideo = {
 
     /**
      * Understand your channel URLs: https://support.google.com/youtube/answer/6180214?hl=en
+     * Channel ID vs Name: https://stackoverflow.com/questions/27974296/how-to-tell-the-difference-between-a-channel-id-and-youtube-username
      */
-    getYoutubeChannelName: function (videoUrl) {
+    getYoutubeChannelId: function (videoUrl) {
         var path = csVideo.parseUrl(videoUrl).pathname;
-        return path.substr(path.lastIndexOf('/') + 1);
+        var urlId = path.substr(path.lastIndexOf('/') + 1);
+        if (urlId.match(/^(UC|HC)[A-Za-z0-9]+$/i)) {
+            return urlId;
+        }
+        else {
+            console.log("Custom Channel Name or User Name: ", urlId);
+            //to do: get channelId from channel custom name or user name
+            return csVideo.testChannelId;
+            //return null;
+        }
     },
 
     getYoutubeLiveStreamUrl: function (videoUrl, success, fail) {
-        var channel = csVideo.getYoutubeChannelName(videoUrl);
-        var liveStreamUrl = null;
-        if (channel.length) {
-            console.log("Youtube API to fetch Live stream url from: " + channel);
-            /**
-             * Youtube API implementation - to do
-             */
-            liveStreamUrl = 'https://www.youtube.com/embed/YE7VzlLtp-4';
-            if (liveStreamUrl) {
-                csVideo.loadLiveStream(liveStreamUrl)
-            }
-            else {
-                csVideo.showChannelBtn(videoUrl);
-            }
+        var channelId = csVideo.getYoutubeChannelId(videoUrl);
+        if (channelId) {
+            console.log("Detected Channel Id: " + channelId);
+            var apiUrl = `${csVideo.youtubeApiUrl}search?part=snippet&channelId=${channelId}&type=video&eventType=live&key=${csVideo.apiKey}`;
+            $.get(apiUrl, function (json) {
+                if (json.items.length) {
+                    var videoId = json.items[0].id.videoId;
+                    console.log("Live Video Id: ", videoUrl);
+                    var liveStreamUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+                    csVideo.loadLiveStream(liveStreamUrl)
+                }
+                else {
+                    csVideo.showChannelBtn(videoUrl);
+                }
+            })
+                .fail(function (json) {
+                    console.log("API call failed: ", json);
+                    csVideo.showChannelBtn(videoUrl);
+                });
         }
+        else {
+            csVideo.showChannelBtn(videoUrl);
+        }
+
     },
 
     onWatch: function ($e) {
@@ -356,19 +382,31 @@ var csVideo = {
     },
 
     loadLiveStream: function (liveStreamUrl) {
-        $("#videoSpinner").removeClass('d-flex').hide();
-        $("#ytPlayer").attr("src", liveStreamUrl);
+        $("#videoSpinner").removeClass('d-flex').hide(700, function () {
+            $("#videoModal .modal-body").append(`
+                <iframe id="ytPlayer" src="${liveStreamUrl}" type="text/html" width="100%" height="320px" frameborder="0" allowfullscreen>
+            `);
+        });
     },
 
     showChannelBtn: function (videoUrl) {
-        $("#videoSpinner").removeClass('d-flex').hide();
-        $("#ytPlayer").hide();
-        alert("Youtube Channel: " + videoUrl);
+        $("#videoSpinner").removeClass('d-flex').hide(700, function () {
+            $("#videoModal #visitChannel").show();
+        });
+    },
 
+    resetYoutubeModal: function () {
+        $("#videoModal #ytPlayer").remove();
+        $("#videoModal #visitChannel").hide();
+        $("#videoModal #videoModalLabel, #videoModal #visitChannelName").text('');
+        $("#videoModal #visitChannelLink").removeAttr('href');
     },
 
     openYoutubeModal: function (url, title) {
         csVideo.videoModal.find("#videoModalLabel").text(title);
+        $("#videoModal #visitChannelName").text(title);
+        $("#videoModal #visitChannelLink").attr('href', url);
+        $("#videoModal #visitChannel").hide();
         csVideo.videoModal.modal("show");
         csVideo.getYoutubeLiveStreamUrl(url);
     }
